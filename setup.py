dd7472fa905b3910b01bfd062e03f418e60909f7
Javier <javier.rodriguez@bluetab.net>
2020-08-17 09:05:04 +0200

    Moved readme to rst file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="awstriggers-jevyanj",
+    name="awstriggers",
     version="0.0.1",
     author="JevyanJ",
     author_email="jjrg184@gmail.com",
